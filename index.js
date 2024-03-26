811f2a3389bb4fb8944364f5ad2ab7315b6c449b
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-03-26 11:30:16 +0500

    Changes_commited_first.

---
+++ index.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "checkNumber",
+        type: "number",
+        message: "Enter your number to check if it is even or odd"
+    }
+]);
+console.log(answer.checkNumber % 2 === 0 ? "Even" : "Odd");
